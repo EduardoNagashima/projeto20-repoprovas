57eb506ba7840593292b8028b14fa3b312681f7b
Eduardo Nagashima <eduyudji@gmail.com>
2022-07-20 15:13:17 -0300

    create project

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import  express, { json }  from "express";
+import "express-async-errors";
+import dotenv from "dotenv";
+import router from "./routers/index.js";
+import errorHandler from "./middlewares/errorHandlerMiddleware.js";
+dotenv.config();
+
+const app = express();
+
+app.use(json());
+app.use(router);
+app.use(errorHandler);
+
+const PORT = +process.env.PORT || 5000;
+app.listen(PORT, ()=>{
+    console.log(`Server running at port ${PORT}`);
+});
---
+++ src/middlewares/errorHandlerMiddleware.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from "express";
+
+export default function errorHandler(error, req: Request, res: Response, next: NextFunction) {
+    let statusCode = 500;
+    if (error.type) {
+        if (error.type === 'BAD_REQUEST') statusCode = 400;
+        if (error.type === 'NOT_FOUND') statusCode = 404;
+        if (error.type === 'CONFLICT') statusCode = 409;
+        if (error.type === 'UNAUTHORIZED') statusCode = 401;
+        return res.status(statusCode).send(error.message);
+    }
+
+    res.status(statusCode).send('Houve algum problema na aplicação, por favor tente novamente.');
+}
